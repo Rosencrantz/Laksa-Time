f0dac6490c622a408176d3900ab015e123e0d740
Steve Haffenden <steve.haffenden@gmail.com>
2011-05-17 15:32:24 +1000

    More hackery

---
+++ tock.js
@@ -0,0 +1,69 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+<head> 
+    <meta charset="utf-8" /> 
+    <title>Is it laksa time yet?</title> 
+    <style type="text/css"> 
+    body { background: white; color: black; text-align: center; font-family: sans-serif; }
+    .assistive { position: absolute; left: -5000px; width: 4000px; overflow: hidden; top: 0; height: 0; }
+    #isitlaksatime { font-size: 10em; margin: 10% 5% 0 5%; text-shadow: 3px 3px 3px #ddd; }
+    .long #isitlaksatime { font-size: 6em; }
+    </style> 
+ 
+</head> 
+<body> 
+ 
+    <h1 class="assistive">is it laksa time yet?</h1> 
+    
+    <p id="isitlaksatime"> 
+        <noscript><a href="http://twitter.com/home/?status=Oi%20@rioter,%20is%20it%20laksa%20time%20yet?%20http://isitlaksatimeyet.200ok.com.au/">Ask Jared</a>.</noscript> 
+    </p> 
+ <script>
+(function(options) {
+    function Tock() {
+    }
+    /*this.event = options.event || 'Beer O\'clock';
+    this.start = options.startDate || Date.now();
+    this.end = options.endDate || Date.now();
+    this.duration = options.duration || 60;
+    this.recurring = options.recurring || false;
+    this.recurrence = options.every || 'once';
+    
+    Date.prototype.isItNow = function() {
+        var time = this.getTime();
+                
+        if(time >= this.start && time <= this.end) {
+            return true;
+        }
+        return false;
+    }
+
+    Date.prototype.isItToday = function() {
+        var day = this.getDay();
+
+        if(day == this.time.getDay()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    Date.prototype.howLong = function() {
+        var laksaDate = this.getNextLaksaDate();
+        
+        return(this || laksaDate - this);     
+    }
+
+    Date.prototype.getNextTime = function() {
+        var laksaDate = new Date(),
+            diff = (4 - this.getDay() + 7) % 7;
+
+        return this.addDays((diff === 0) ? diff += 7 : diff); 
+    }
+
+    return Date.now();*/
+})();
+</script>
+
+</body> 
+</html> 
